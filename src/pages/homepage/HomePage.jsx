585f1a5ca52ee356179b17f9bb8423ef296bcc60
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-08-03 22:10:45 +0700

    updated

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <div className="bg-gray-50 min-h-screen text-gray-800 transition-colors duration-300">
             <Navbar />
             <section className="max-w-7xl mx-auto px-4 pt-2">
-                <div>isi konten disini</div>
+                <div>isi konten disinis</div>
             </section>
             <Footer />
         </div>
